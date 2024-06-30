9560f34bd4ce9bfd85fe0f6f352bdfb99594d279
jugggernault <abedneggognanguenon@gmail.com>
2024-06-30 20:22:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const card = document.querySelector('.first');
+// const desc = document.querySelector('.desc');
+
+// card.addEventListener('mouseenter', () => {
+//     desc.style.display = 'flex';
+//     card.style.display = 'none'
+// });
+
+// card.addEventListener('mouseleave', () => {
+//     desc.style.display = 'none';
+//     card.style.display = 'flex'
+// });
